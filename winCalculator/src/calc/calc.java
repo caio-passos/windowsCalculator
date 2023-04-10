73311b73f2f01a2318dcf09a61645cecab21ffd6
Caio Passos <caiopss99@gmail.com>
2023-04-10 17:03:16 -0300

    The main layout of the program

---
+++ winCalculator/src/calc/calc.java
@@ -0,0 +1,37 @@
+package calc;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lab. de Software
+ */
+public class calc extends GUI implements ActionListener{
+    JPanel extends GUI();
+    private JFrame;
+    
+   public resultado(bAC, bC, bApagar, bDivisao, b9, b8, b7, b6, b5, b4, b3, b2, b1, b0, bPonto){
+       b0 = 0;
+       b1 = 1;
+       b2 = 2; 
+       b3 = 3;
+       b4 = 4;
+       b5 = 5;
+       b6 = 6;
+       b7 = 7;
+       b8 = 8;
+       b9 = 9;
+       
+       
+   }
+   
+   public mostrar(painel, bAC, bC, bApagar, bDivisao, b9, b8, b7, b6, b5, b4, b3, b2, b1, b0, bPonto){
+    
+}
+   
+   calculator calc = new calculator();
+    
+}
