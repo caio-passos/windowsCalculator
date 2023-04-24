3368cbd5dfb471ae166b54056c61d70967bb66c8
CaioP <caiopss99@gmail.com>
2023-04-23 22:36:38 -0300

    Version 1.0. Functional with not known bugs

---
+++ dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"CalculadoraDesignR","l":"calcula()"},{"p":"<Unnamed>","c":"CalculadoraDesignR","l":"CalculadoraDesignR()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"CalculadoraDesignR","l":"converte1()"},{"p":"<Unnamed>","c":"CalculadoraDesignR","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
